467b5473f14551cd749b7f0b72b50f0866102f90
Bekzat <kalibekzat2003@gmail.com>
2024-05-04 16:44:17 +0500

    small change

---
+++ src/app/components/PhrasalVerbsPagination.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const PhrasalVerbsPagination = (props: Props) => {
+  return (
+    <div>PhrasalVerbsPagination fff</div>
+  )
+}
+
+export default PhrasalVerbsPagination
